f894d10715c342f6dacee7f9d1e64b65ef89e91d
markom1 <44947815+markom1@users.noreply.github.com>
2019-01-16 17:37:35 +0100

    Otpuštanje objekata pomoću naredbe using
    + StreamWriter

---
+++ MijajlovicPisanjeDatotekaStreamWriter/MijajlovicPisanjeDatotekaStreamWriter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace MijajlovicPisanjeDatotekaStreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite ime: ");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+
+            // Podatke upisujemo u datoteku
+            Console.WriteLine("\n-- Zapisujemo u datoteku...");
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Ucenik\Documents\datoteka.txt"))
+            {
+                sw.WriteLine("Ime: {0}", ime);
+                sw.WriteLine("Prezime: {0}", prezime);
+            }
+
+            // Podatke upisujemo u datoteku
+            Console.WriteLine("\n-- Pročitano iz datoteke: ");
+            using (StreamReader sr = new StreamReader(@"C:\Users\Ucenik\Documents\datoteka.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MijajlovicPisanjeDatotekaStreamWriter/MijajlovicPisanjeDatotekaStreamWriter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MijajlovicPisanjeDatotekaStreamWriter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MijajlovicPisanjeDatotekaStreamWriter")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f8f99cd-8e88-44cb-9316-a94593da587f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
