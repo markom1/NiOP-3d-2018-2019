20606de1ef32a41797c9c822c5723fb29b22d53a
markom1 <44947815+markom1@users.noreply.github.com>
2019-01-16 17:26:56 +0100

    Čitanje datoteka (StreamReader)

---
+++ MijajlovicCitanjeDatoteka/MijajlovicCitanjeDatoteka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace MijajlovicCitanjeDatoteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Ucenik\Documents\datoteka.txt");
+
+            // Čitanje datoteke liniju po liniju
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            // Zatvaramo datoteku
+            sr.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
