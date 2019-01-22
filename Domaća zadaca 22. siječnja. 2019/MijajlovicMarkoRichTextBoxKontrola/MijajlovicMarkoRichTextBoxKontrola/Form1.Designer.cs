a9b94db71d12353d3a2b37751f654fdd9b530fab
markom1 <44947815+markom1@users.noreply.github.com>
2019-01-22 16:30:08 +0100

    Add files via upload

---
+++ Domaća zadaca 22. siječnja. 2019/MijajlovicMarkoRichTextBoxKontrola/MijajlovicMarkoRichTextBoxKontrola/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MijajlovicMarkoRichTextBoxKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCitaj = new System.Windows.Forms.Button();
+            this.btnBrisi = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.rtxtBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oProgramuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCitaj
+            // 
+            this.btnCitaj.Location = new System.Drawing.Point(12, 34);
+            this.btnCitaj.Name = "btnCitaj";
+            this.btnCitaj.Size = new System.Drawing.Size(75, 23);
+            this.btnCitaj.TabIndex = 0;
+            this.btnCitaj.Text = "Citaj";
+            this.btnCitaj.UseVisualStyleBackColor = true;
+            this.btnCitaj.Click += new System.EventHandler(this.btnCitaj_Click);
+            // 
+            // btnBrisi
+            // 
+            this.btnBrisi.Location = new System.Drawing.Point(179, 34);
+            this.btnBrisi.Name = "btnBrisi";
+            this.btnBrisi.Size = new System.Drawing.Size(75, 23);
+            this.btnBrisi.TabIndex = 1;
+            this.btnBrisi.Text = "Brisi";
+            this.btnBrisi.UseVisualStyleBackColor = true;
+            this.btnBrisi.Click += new System.EventHandler(this.btnBrisi_Click);
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(12, 228);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(75, 23);
+            this.btnSpremi.TabIndex = 2;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // rtxtBox
+            // 
+            this.rtxtBox.Location = new System.Drawing.Point(12, 73);
+            this.rtxtBox.Name = "rtxtBox";
+            this.rtxtBox.Size = new System.Drawing.Size(242, 139);
+            this.rtxtBox.TabIndex = 3;
+            this.rtxtBox.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(278, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oProgramuToolStripMenuItem
+            // 
+            this.oProgramuToolStripMenuItem.Name = "oProgramuToolStripMenuItem";
+            this.oProgramuToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.oProgramuToolStripMenuItem.Text = "O programu";
+            this.oProgramuToolStripMenuItem.Click += new System.EventHandler(this.oProgramuToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 271);
+            this.Controls.Add(this.rtxtBox);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.btnBrisi);
+            this.Controls.Add(this.btnCitaj);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Upotreba richTextBox kontrole";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCitaj;
+        private System.Windows.Forms.Button btnBrisi;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.RichTextBox rtxtBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oProgramuToolStripMenuItem;
+    }
+}
+
---
+++ Domaća zadaca 22. siječnja. 2019/MijajlovicMarkoRichTextBoxKontrola/MijajlovicMarkoRichTextBoxKontrola/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MijajlovicMarkoRichTextBoxKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            // spremanje sadrzaja rtxtBoxa u datoteku
+            rtxtBox.SaveFile(@"C:Tempproba.rtf");
+            MessageBox.Show("Tekst spremljen.");
+        }
+
+        private void btnCitaj_Click(object sender, EventArgs e)
+        {
+            // učitavanje teksta iz datoteke u rtxtBox
+            rtxtBox.LoadFile(@"C:Tempproba.rtf");
+        }
+
+        private void btnBrisi_Click(object sender, EventArgs e)
+        {
+            // brisanje sadrzaja rtxtBoxa
+            rtxtBox.Clear();
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreativeCommonsLicense oProgramu = new CreativeCommonsLicense();
+            oProgramu.Show();
+        }
+    }
+}
