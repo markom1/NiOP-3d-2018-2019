bd722044c1c577cd49a588161fd3098681ab08b5
markom1 <44947815+markom1@users.noreply.github.com>
2019-01-22 16:33:38 +0100

    Add files via upload

---
+++ Domaća zadaca 22. siječnja. 2019/MijajlovicMarkoPictureBoxKontrola/MijajlovicMarkoPictureBoxKontrola/CreativeCommonsLicense.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MijajlovicMarkoPictureBoxKontrola
+{
+    public partial class CreativeCommonsLicense : Form
+    {
+        public CreativeCommonsLicense()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZatvori_Click(object sender, EventArgs e)
+        {
+            //Zatvara prozor koji govori o programu.
+
+            this.Close();
+        }
+
+        private void linkLabelCreativeCommons_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //Label koji vodi na link unutar zagrada.
+
+            System.Diagnostics.Process.Start("https://creativecommons.org/licenses/by-nc-sa/4.0/");
+        }
+    }
+}
---
+++ Domaća zadaca 22. siječnja. 2019/MijajlovicMarkoPictureBoxKontrola/MijajlovicMarkoPictureBoxKontrola/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MijajlovicMarkoPictureBoxKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // klikom na gumb, slika postaje vidljiva
+            pictureBox1.Visible = true;
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreativeCommonsLicense oProgramu = new CreativeCommonsLicense();
+            oProgramu.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
