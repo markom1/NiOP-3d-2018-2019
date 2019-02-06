d95246f931dbc82ea52b1407bdbd064a0543adb1
markom1 <44947815+markom1@users.noreply.github.com>
2019-02-06 12:10:09 +0100

    Klase, konstruktori, get i set

---
+++ MijajlovicMarkoKlase/MijajlovicMarkoKlase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MijajlovicMarkoKlase.Klase;
+
+namespace MijajlovicMarkoKlase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo avion = new Vozilo();
+
+            avion.setBrojKotaca(6);
+            avion.setLeti(true);
+            avion.setPliva(false);
+            avion.setVozi(true);
+            avion.setVrsta("Cessna");
+            avion.setOznaka("1312");
+
+            Console.WriteLine("Vrsta: " + avion.getVrsta() + "\nOznaka: " + avion.getOznaka() + "\nBroj Kotaca: " + avion.getBrojKotaca()
+                + "\nPliva: " + avion.getPliva() + "\nVozi: " + avion.getVozi() + "\nLeti: " + avion.getVozi() + "\n");
+
+            Vozilo kamion = new Vozilo(8, false, false, true, "TAM", "RI1987AR");
+
+            Console.WriteLine(kamion.ToString());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
