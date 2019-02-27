93788e13bb47e0778d7f6b08b2471469cf6de596
markom1 <44947815+markom1@users.noreply.github.com>
2019-02-27 18:07:32 +0100

    Add files via upload
    nije dovrseno

---
+++ MijajlovicMarkoZadatakZaKraj/MijajlovicMarkoZadatakZaKraj/Klase/Vozilo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MijajlovicMarkoZadatakZaKraj.Klase
+{
+    class Vozilo
+    {
+        private string vrsta;
+        private int maxBrzina, brojKotaca;
+
+        public void setBrojKotaca(int brojKotaca)
+        {
+            this.brojKotaca = brojKotaca;
+        }
+
+        public int getBrojKotaca()
+        {
+            return this.brojKotaca;
+        }
+
+        public void setMaxBrzinaMotora(int maxBrzina)
+        {
+            this.maxBrzina = maxBrzina;
+        }
+
+        public int getMaxBrzinaMotora()
+        {
+            return this.maxBrzina;
+        }
+
+        public void setVrsta(string vrsta)
+        {
+            this.vrsta = vrsta;
+        }
+
+        public string getVrsta()
+        {
+            return this.vrsta;
+        }
+       
+        public void unosPodataka()
+        {
+            Console.WriteLine("Unesite broj kotaca: ");
+            setBrojKotaca(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Unesite vrstu vozila: ");
+            setVrsta(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return "Vrsta: " + vrsta +
+                "Broj kotaca: " + brojKotaca +
+                "Maksimalna brzina: " + maxBrzina;
+        }
+
+        public bool carCheck()
+        {
+            if (brojKotaca == 4 && vrsta == "Automobil" || vrsta == "Auto" || vrsta == "automobil" || vrsta == "auto")
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void carMultiplier()
+        {
+            double multiplied;
+
+            multiplied = (maxBrzina * 0.03) * 100;
+
+           // Console.WriteLine(multiplied.ToString());
+
+            Console.WriteLine("Vrsta: " + vrsta +
+                "Broj kotaca: " + brojKotaca +
+                "Maksimalna Brzina" + multiplied);
+
+        }
+    }
+
+    
+}
---
+++ MijajlovicMarkoZadatakZaKraj/MijajlovicMarkoZadatakZaKraj/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MijajlovicMarkoZadatakZaKraj.Klase;
+
+namespace MijajlovicMarkoZadatakZaKraj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo vozilo = new Vozilo();
+            Vozilo motor = new Vozilo();
+
+            
+
+            if (vozilo.carCheck() == true)
+            {
+                vozilo.unosPodataka();
+                vozilo.carMultiplier();
+            }
+            else vozilo.ToString();
+
+        }
+    }
+}
